3e7e4d753de5b1c15938c66c3817b78e04858eec
Mike Lara <larami100@hotmail.com>
2019-05-17 16:36:54 -0700

    * Game mode modified to add Game Timer and Menu Widget

--- Source/BombermanUE4/BombermanUE4GameMode.cpp
+++ Source/BombermanUE4/BombermanUE4GameMode.cpp
@@ -4,6 +4,26 @@
 #include "BombermanUE4PlayerController.h"
 #include "BombermanUE4Character.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+#include "BombermanUserWidget.h"
+
+void ABombermanUE4GameMode::DecreaseTimer()
+{
+	//Substract one second to the timer 
+	GameTimer -= FTimespan(0, 0, 1);
+	if (ABombermanUE4Character* Player = Cast<ABombermanUE4Character>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)))
+	{
+		//Set game timer and bombs counter values in the interface 
+		((UBombermanUserWidget*)CurrentWidget)->SetTimeAndBombs(GameTimer, Player->BombsCounter);
+
+		// If the game timer is 0:00
+		if (!GameTimer.GetMinutes() && !GameTimer.GetSeconds())
+		{
+			// End game
+			OnGameOver();
+		}
+	}
+}
 
 ABombermanUE4GameMode::ABombermanUE4GameMode()
 {
@@ -16,4 +36,53 @@ ABombermanUE4GameMode::ABombermanUE4GameMode()
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+
+	//Set the game timer to 5 minutes
+	GameTimer = FTimespan(0, 5, 0);
 }
+
+void ABombermanUE4GameMode::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	//If already exist a widget in the interface
+	if (CurrentWidget != nullptr)
+	{
+		// Remove it from the screen
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
+
+void ABombermanUE4GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+	((UBombermanUserWidget*)CurrentWidget)->Load();
+
+	// Call each second the DecreaseTimer function in order to decrease the game timer
+	GetWorldTimerManager().SetTimer(FuseTimerHandle, this, &ABombermanUE4GameMode::DecreaseTimer, 1.f, true);
+}
+
+void ABombermanUE4GameMode::OnGameOver()
+{
+	// Pause the game
+	UGameplayStatics::SetGamePaused(GetWorld(), true);
+
+	// Display Game over message
+	((UBombermanUserWidget*)CurrentWidget)->OnGameOver();
+}
+
+void ABombermanUE4GameMode::UpdateTimeAndBombs(int32 Bombs)
+{
+	// Update Game timer and bombs values displayed at the screen 
+	((UBombermanUserWidget*)CurrentWidget)->SetTimeAndBombs(GameTimer, Bombs);
+}
+
+
--- Source/BombermanUE4/BombermanUE4GameMode.h
+++ Source/BombermanUE4/BombermanUE4GameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+//#include "Misc/Timespan.h"
 #include "BombermanUE4GameMode.generated.h"
 
 UCLASS(minimalapi)
@@ -11,8 +12,38 @@ class ABombermanUE4GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+	FTimerHandle FuseTimerHandle;
+
+	void DecreaseTimer();
+
 public:
 	ABombermanUE4GameMode();
+
+	UFUNCTION(BlueprintCallable, Category = "UMG Game")
+	void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTimespan GameTimer;
+
+	virtual void BeginPlay() override;
+
+	// Removed for optimization
+	// virtual void Tick(float DeltaTime) override;
+
+	void OnGameOver();
+
+	void UpdateTimeAndBombs(int32 Bombs);
+
+protected:
+
+	FTimerHandle GameTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UMG Game")
+	TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	UPROPERTY()
+	UUserWidget* CurrentWidget;
+
 };
 
 
